71f8a4e05970b2824f1c0dfd28c5eac4dc0e000d
Sumit Jadhav <sumitjadav2525@gmail.com>
2025-02-24 12:07:08 +0530

    routes

--- server.js
+++ server.js
@@ -14,5 +14,29 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/todos', todoRoutes);
 
+// GET method route
+app.get('/', (req, res) => {
+    res.send('GET request to the homepage')
+  })
+  app.get('/login', (req, res) => {
+    res.send('GET request to the login')
+  })
+  
+  app.get('/register', (req, res) => {
+    res.send('GET request to the register')
+  })
+  
+  // POST method route
+  app.post('/', (req, res) => {
+    res.send('POST request to the homepage')
+  })
+
+  app.post('/contact', (req, res) => {
+    res.send('POST request to the contact')
+  })
+
+  app.post('/user', (req, res) => {
+    res.send('POST request to the user')
+  })
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
